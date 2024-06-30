94aba4fe8d75920affa516df644923e017b102b9
Nikhil Patil <nikhil.patil.3632@gmail.com>
2024-06-30 22:14:19 +0200

    Added protocols and findings

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit, inject } from '@angular/core';
 import {
   AutoCompleteCompleteEvent,
   AutoCompleteModule,
+  AutoCompleteSelectEvent,
 } from 'primeng/autocomplete';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import {
   OrderListModule,
   OrderListSelectionChangeEvent,
 } from 'primeng/orderlist';
-import { Protocol, ReportService, Template } from './report.service';
+import { Finding, Protocol, ReportService, Template } from './report.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SplitterModule } from 'primeng/splitter';
+import {
+  PickListModule,
+  PickListSourceFilterEvent,
+  PickListTargetFilterEvent,
+} from 'primeng/picklist';
+import { InputSwitchChangeEvent, InputSwitchModule } from 'primeng/inputswitch';
 
 @Component({
   selector: 'app-report',
@@ -23,6 +30,8 @@ import { SplitterModule } from 'primeng/splitter';
     FloatLabelModule,
     OrderListModule,
     SplitterModule,
+    PickListModule,
+    InputSwitchModule,
   ],
   providers: [ReportService],
   templateUrl: './report.component.html',
@@ -33,12 +42,20 @@ export class ReportComponent implements OnInit {
 
   templates: readonly Template[] = this.reportService.templates;
 
-  protocols: Protocol[] = this.reportService.protocols;
+  protocols: Protocol[] = [];
 
   filteredTemplates: Template[] = [];
 
+  findingList: Map<string, Finding[]> = new Map<string, Finding[]>();
+
+  selectedFindings: Map<string, Finding[]> = new Map<string, Finding[]>();
+
+  currentProtocol: Protocol | undefined;
+
   formGroup: FormGroup = new FormGroup({
     template: new FormControl<Template | null>(null),
+    allowTagSearch: new FormControl<boolean>(true),
+    onlyExactTagSearch: new FormControl<boolean>(false),
   });
 
   ngOnInit() {}
@@ -47,5 +64,95 @@ export class ReportComponent implements OnInit {
     this.filteredTemplates = this.templates.filter((t) =>
       t.name.toLowerCase().includes(event.query.toLowerCase()),
     );
+  }
+
+  onTemplateSelect(event: AutoCompleteSelectEvent): void {
+    this.protocols = this.reportService.protocols.filter(
+      (f) => f.templateId === event.value.id,
+    );
+  }
+
+  onProtocolSelect(event: OrderListSelectionChangeEvent): void {
+    this.currentProtocol = event.value[0];
+
+    const findings = this.reportService.findings.filter(
+      (f) => f.protocolId === event.value[0].id,
+    );
+
+    if (!this.findingList.has(this.currentProtocol!.id)) {
+      this.findingList.set(this.currentProtocol!.id, findings);
+    }
+
+    if (!this.selectedFindings.has(this.currentProtocol!.id)) {
+      this.selectedFindings.set(this.currentProtocol!.id, []);
+    }
+  }
+
+  onAllowTagSearchChange(event: InputSwitchChangeEvent): void {
+    this.triggerFindingsSearch();
+
+    event.checked
+      ? this.formGroup.controls['onlyExactTagSearch'].enable()
+      : this.formGroup.controls['onlyExactTagSearch'].disable();
+
+    if (event.checked) {
+      return;
+    }
+
+    this.formGroup.controls['onlyExactTagSearch']?.setValue(false);
+  }
+
+  onExactTagSearchChange(_: InputSwitchChangeEvent): void {
+    this.triggerFindingsSearch();
+  }
+
+  private triggerFindingsSearch(): void {
+    document
+      .querySelector('input[data-pc-section="sourceFilterInput"]')
+      ?.dispatchEvent(new Event('keyup'));
+
+    document
+      .querySelector('input[data-pc-section="targetFilterInput"]')
+      ?.dispatchEvent(new Event('keyup'));
+  }
+
+  onAvailableFindingsFilter(event: PickListSourceFilterEvent): void {
+    this.findingsFilter(event, this.findingList);
+  }
+
+  onSelectedFindingsFilter(event: PickListTargetFilterEvent): void {
+    this.findingsFilter(event, this.selectedFindings);
+  }
+
+  private findingsFilter(
+    event: PickListSourceFilterEvent | PickListTargetFilterEvent,
+    findingsMap: Map<string, Finding[]>,
+  ) {
+    if (
+      !event.query ||
+      !this.formGroup.controls['allowTagSearch']!.getRawValue()
+    ) {
+      return;
+    }
+
+    const moreFindings = findingsMap
+      .get(this.currentProtocol!.id)!
+      .filter((f) =>
+        f.tags?.some((s) => {
+          return this.formGroup.controls['onlyExactTagSearch']!.getRawValue()
+            ? s === event.query
+            : s.includes(event.query!);
+        }),
+      );
+
+    if (!moreFindings?.length) {
+      return;
+    }
+
+    for (const item of moreFindings) {
+      if (!event.value?.find((f) => f.id === item.id)) {
+        event.value?.push(item);
+      }
+    }
   }
 }
--- src/app/report/report.service.ts
+++ src/app/report/report.service.ts
@@ -16,6 +16,7 @@ export interface Finding {
   readonly id: string;
   readonly description: string;
   readonly tags?: string[];
+  readonly protocolId: string;
 }
 
 @Injectable()
@@ -27,7 +28,7 @@ export class ReportService {
     },
     {
       id: '2',
-      name: 'MRI Brain',
+      name: 'MRI Cervical Spine',
     },
     {
       id: '3',
@@ -67,34 +68,158 @@ export class ReportService {
       templateId: '1',
     },
     {
-      id: '1',
+      id: '6',
       name: 'Protocol',
       order: 1,
       templateId: '2',
     },
     {
-      id: '2',
+      id: '7',
       name: 'Patient Info',
       order: 2,
       templateId: '2',
     },
     {
-      id: '3',
-      name: 'Frontal Lobe',
+      id: '8',
+      name: 'Disc C4/5',
       order: 3,
       templateId: '2',
     },
     {
-      id: '4',
-      name: 'Temporal Lobe',
+      id: '9',
+      name: 'Disc C6/7',
       order: 4,
       templateId: '2',
     },
     {
-      id: '1',
+      id: '10',
       name: 'Protocol',
       order: 1,
       templateId: '3',
+    },
+  ];
+
+  findings: Finding[] = [
+    {
+      id: '1',
+      description:
+        'Plain and contrast CT section of the abdomen have been obtained',
+      tags: ['protocol', 'liver', 'liver protocol'],
+      protocolId: '1',
+    },
+    {
+      id: '2',
+      description: 'Patient Name:        Age:            ',
+      tags: ['patient', 'patient information'],
+      protocolId: '2',
+    },
+    {
+      id: '3',
+      description: 'Liver appears normal in size and shape',
+      tags: ['normal'],
+      protocolId: '3',
+    },
+    {
+      id: '4',
+      description:
+        'CT demonstrates of hyperechoic lesion with attenuation of the beam suggesting possible fat content',
+      tags: ['adenoma', 'hepatic', 'hepatic adenoma'],
+      protocolId: '3',
+    },
+    {
+      id: '5',
+      description:
+        'CT demonstrates central hemorrhagic portions of cavernous hemangiomas remain non-enhancing',
+      tags: ['hemangioma', 'hepatic', 'hepatic hemangioma'],
+      protocolId: '3',
+    },
+    {
+      id: '6',
+      description:
+        'Small focal hepatocellular carcinoma appears hypoechoic compared with normal liver',
+      tags: ['HCC', 'Hepatocellular carcinoma', 'carcinoma', 'hepato'],
+      protocolId: '3',
+    },
+    {
+      id: '7',
+      description: 'Predominantly portal venous supply to dysplastic nodules',
+      tags: ['cirrhosis'],
+      protocolId: '3',
+    },
+    {
+      id: '8',
+      description: 'Gall bladder is distended and appears normal',
+      tags: ['normal'],
+      protocolId: '4',
+    },
+    {
+      id: '9',
+      description:
+        'The CT features of acute cholecystitis include gallstones, thickening of the gallbladder wall, pericholecystic fluid, stranding of the pericholecystic fat, high-attenuation bile, and blurring of the interface between the gallbladder and the liver',
+      tags: ['Cholecystitis', 'stone'],
+      protocolId: '4',
+    },
+    {
+      id: '10',
+      description:
+        'Gallbladder carcinoma is a mass that fills most of an enlarged and deformed',
+      tags: ['Carcinoma'],
+      protocolId: '4',
+    },
+    {
+      id: '11',
+      description: 'Both kidneys appears normal in size',
+      tags: ['normal'],
+      protocolId: '5',
+    },
+    {
+      id: '12',
+      description:
+        'Contrast-enhanced CT scan at corticomedullary phase shows fistula ',
+      tags: ['Iatrogenic'],
+      protocolId: '5',
+    },
+    {
+      id: '13',
+      description: 'Sagittal T2W scan of the cervical spine was performed ',
+      tags: ['protocol', 'cervical', 'spine', 'cervical spine'],
+      protocolId: '6',
+    },
+    {
+      id: '14',
+      description: 'Patient Name:        Age:            ',
+      tags: ['patient', 'patient information'],
+      protocolId: '7',
+    },
+    {
+      id: '15',
+      description: 'Appears normal',
+      tags: ['normal'],
+      protocolId: '8',
+    },
+    {
+      id: '16',
+      description: 'Posterior disc bulges are seen at C4/5',
+      tags: ['bulges', 'posterior'],
+      protocolId: '8',
+    },
+    {
+      id: '17',
+      description: 'Appears normal',
+      tags: ['normal'],
+      protocolId: '9',
+    },
+    {
+      id: '18',
+      description: 'Moderate stenosis with compression of spinal cord',
+      tags: ['stenosis', 'compression'],
+      protocolId: '9',
+    },
+    {
+      id: '19',
+      description: 'Some protocol performed',
+      tags: ['protocol'],
+      protocolId: '10',
     },
   ];
 }
